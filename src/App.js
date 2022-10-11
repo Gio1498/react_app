0f7d4a3f363c7a0d738ff345cdc7bb506bdbd69d
Gio1498 <giovanni.messina1498@gmail.com>
2022-10-11 17:07:11 +0200

    End of anime database
    End of anime database, but search field not work cause api is down at the moment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [animeList, SetAnimeList] = useState([]);
+    const [topAnime, SetTopAnime] = useState([]);
+    const [search, SetSearch] = useState("");
+
+    const GetTopAnime = async () => {
+        const temp = await fetch(`https://api.jikan.moe/v4/top/anime`)
+            .then(res => res.json());
+        SetTopAnime(temp.data.slice(0, 5));
+    }
+
+    const HandleSearch = (e) => {
+        e.preventDefault();
+        FetchAnime(search);
+    }
+
+    const FetchAnime = async (search) => {
+        alert("API is down, so this won't work"); // form more info: https://docs.api.jikan.moe/#tag/anime/operation/getAnimeSearch
+        // const temp = await fetch(`https://api.jikan.moe/v4/search/anime?q=${search}`)
+        //     .then(res => res.json());
+        // SetAnimeList(temp.results);
+    }
+
+    useEffect(() => {
+        GetTopAnime();
+    }, []);
+
+
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Header />
+                <div className="content-wrap">
+                    <Sidebar topAnime={topAnime} />
+                    <MainContent
+                        HandleSearch={HandleSearch}
+                        search={search}
+                        SetSearch={SetSearch}
+                        animeList={animeList} />
+                </div>
+                <hr />
+                <div className="row">
+
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
